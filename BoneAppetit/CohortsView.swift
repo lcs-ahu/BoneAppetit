57d2ad145511e6428a8d21bef34d2e19deddcadc
Ann <ahu@lcs.on.ca>
2026-02-26 09:53:06 -0500

    finished

--- BoneAppetit/CohortsView.swift
+++ BoneAppetit/CohortsView.swift
@@ -9,15 +9,53 @@ import SwiftUI
 
 struct CohortsView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            VStack {
+                Image(systemName: "person.crop.circle.badge.questionmark.fill")
+                    .resizable()
+                    .frame(width: 150, height: 130);
+                Text("You are not a member of any cohorts")
+                    .font(.title)
+                    .multilineTextAlignment(.center)
+                    .padding()
+                Text("A cohort is a group of people who care for one or more pets")
+                    .multilineTextAlignment(.center)
+                    .padding()
+                Text("Creat a new cohort by pressing + or open the app later on to see if you have received an invitation")
+                    .multilineTextAlignment(.center)
+                .padding()            }
+            .padding()
+            .navigationTitle("Cohorts")
+            .toolbar {
+                ToolbarItem(placement: .topBarLeading) {
+                    Button {
+                        // code goes here
+                    } label: {
+                        Text("Sign Out")
+                    }
+                    
+                }
+                
+                ToolbarItem(placement: .topBarTrailing) {
+                    Button {
+                        // code goes here
+                    } label: {
+                        Image(systemName:
+                                "plus")
+                        
+                    }
+                    .buttonStyle(.borderedProminent)
+                    
+                    
+                    
+                }
+            }
         }
-        .padding()
     }
+    
+    
 }
+
 
 #Preview {
     CohortsView()
