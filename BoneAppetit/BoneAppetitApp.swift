7fb175128c8affea7551d120798151a653f6977d
Ann <ahu@lcs.on.ca>
2026-02-26 09:03:01 -0500

    Initial Commit

---
+++ BoneAppetit/BoneAppetitApp.swift
@@ -0,0 +1,17 @@
+//
+//  BoneAppetitApp.swift
+//  BoneAppetit
+//
+//  Created by ANN HU on 2026/2/26.
+//
+
+import SwiftUI
+
+@main
+struct BoneAppetitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
